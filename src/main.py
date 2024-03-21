ebeb5dda10c0ee8726171ab6699705ddeaaf226f
mpodlesn <m.podlesna@stud.elka.pw.edu.pl>
2024-03-21 17:30:58 +0100

    test v2

--- src/main.py
+++ src/main.py
@@ -1,19 +1,41 @@
+from miditok import REMI, TokenizerConfig
 from pathlib import Path
+from miditok.pytorch_data import DatasetTok, DataCollator
+from miditok.data_augmentation import augment_midi_dataset
+from torch.utils.data import DataLoader
 
-from miditok import REMI, TokSequence
-from copy import deepcopy
+TOKENIZER_PARAMS = {
+    "pitch_range": (21, 109),
+    "beat_res":{(0,4): 8, (4, 12): 4},
+    "use_chords": True,
+}
 
-tokenizer = REMI()  # using defaults parameters (constants.py)
-paths_midis = list(Path("../data/maestro-v3.0.0-midi/maestro-v3.0.0").glob('**/*.mid'))
+config = TokenizerConfig(**TOKENIZER_PARAMS)
 
-# Learns the vocabulary with BPE
-tokenizer.learn_bpe(
-    vocab_size=500,
-    files_paths=paths_midis,
+#create the tokenizer
+tokenizer = REMI(config)
+
+midi_paths = list("data/maestro-v3.0.0-midi/maestro-v3.0.0").glob("**/*.mid")
+
+#build the vocabulary (no of unique signs in sequences?) with Byte Pair Encoding
+tokenizer.learn_bpe(vocab_size=1000,
+                    files_paths=midi_paths)
+#SUGGESTION: vocab size as an editable param for user
+
+dataset = DatasetTok(
+    files_paths=midi_paths,
+    min_seq_len=100,
+    max_seq_len=1024,
+    tokenizer=tokenizer,
 )
 
-# Opens tokens, apply BPE on them, and decode BPE back
-# tokens = tokenizer.load_tokens(tokens_no_bpe_paths[0])
-tokens = TokSequence(ids=tokens)
-tokens_with_bpe = tokenizer.apply_bpe(deepcopy(tokens))  # copy as the method is inplace
-tokens_no_bpe = tokenizer.decode_bpe(deepcopy(tokens_with_bpe))
+#form a batch
+collator = DataCollator(
+    tokenizer["PAD_none"], tokenizer["BOS_none"], tokenizer["EOS_none"]
+)
+
+data_loader = DataLoader(dataset=dataset, collate_fn=collator)
+
+# for batch in data_loader: train
+
+

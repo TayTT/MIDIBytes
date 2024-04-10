9e2d0ba79fbbc9b0a92a3d5d2e1c7e19fb3ed00a
Kamil Adam Jabłoński <82217577+jab3onski@users.noreply.github.com>
2024-04-10 16:42:51 +0200

    Streamlit base theme can be modified now, cosmetic changes

--- src/app/app_streamlit.py
+++ src/app/app_streamlit.py
@@ -1,3 +1,16 @@
 import streamlit as st
 
-st.write("Hello world")
+st.set_page_config(
+    page_title="MIDIBytes",
+    page_icon="🎶",
+    layout="centered",
+    menu_items={
+        'About': "This is a header. This is an *extremely* cool app!"
+    }
+)
+
+def main():
+    st.text("Hello world")
+
+if __name__ == "__main__":
+    main()

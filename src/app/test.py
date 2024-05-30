24bcb0563744a17bbd8160629e2f82274669f432
Tay <marlenapodlesna@gmail.com>
2024-05-30 22:41:40 +0200

    prep for hgf

--- src/app/test.py
+++ src/app/test.py
@@ -2,6 +2,7 @@ from config import EXAMPLE_DIR
 from tokenizer_config import TokenizerConfigBuiler
 # from src.tokenizer_option import TokenizerOption
 from get_midi import MidiReader
+from tokenizer_config import TokenizerConfigBuilder as tcb
 # import mido
 '''
 Test file for the get_midi, tokenizer_config files
@@ -26,3 +27,8 @@ print(tokens)
 
 #convert tokens back to MIDI and see differences
 back_to_MIDI = tokenizer.tokens_to_MIDI(tokens)
+
+audio_path = 'data/generated'
+audio_name = 'hgf1'
+
+melody = tcb.midi_to_audio(back_to_MIDI, audio_path, audio_name)
